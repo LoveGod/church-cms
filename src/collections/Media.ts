27997f81a798aa964a3486b066b73752cddfad94
Afi Amouzou <afi.amouzou@burda-forward.de>
2026-05-25 15:51:50 +0200

    #12: Configure media collection

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,9 +1,43 @@
 import type { CollectionConfig } from 'payload'
+import { isAdmin } from './access/admins'
+import { isAdminOrEditor } from './access/adminOrEditor'
 
 export const Media: CollectionConfig = {
   slug: 'media',
   access: {
     read: () => true,
+    create: ({ req: { user } }) => isAdminOrEditor({ req: { user } }),
+    update: ({ req: { user } }) => isAdminOrEditor({ req: { user } }),
+    delete: ({ req: { user } }) => isAdmin({ req: { user } }),
+  },
+  upload: {
+    staticDir: 'media',
+    adminThumbnail: 'thumbnail',
+    mimeTypes: ['image/*'],
+    imageSizes: [
+      {
+        name: 'thumbnail',
+        width: 400,
+        height: 300,
+        position: 'centre',
+      },
+      {
+        name: 'card',
+        width: 768,
+        height: 1024,
+        position: 'centre',
+      },
+      {
+        name: 'tablet',
+        width: 1024,
+        // By specifying `undefined` or leaving a height undefined,
+        // the image will be sized to a certain width,
+        // but it will retain its original aspect ratio
+        // and calculate a height automatically.
+        height: undefined,
+        position: 'centre',
+      },
+    ],
   },
   fields: [
     {
@@ -12,5 +46,4 @@ export const Media: CollectionConfig = {
       required: true,
     },
   ],
-  upload: true,
 }
